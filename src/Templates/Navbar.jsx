72010de35b5b7a5d3da98d68b9fb944ccdbc00af
philemoncobbina <philemoncobbina19@gmail.com>
2024-11-09 15:19:13 +0000

    tesing

--- src/Templates/Navbar.jsx
+++ src/Templates/Navbar.jsx
@@ -57,7 +57,7 @@ const Navbar = () => {
                 href="/"
                 className={`${getLinkClass('/')} px-3 py-2 rounded-md text-sm font-medium no-underline`}
               >
-                Home
+                Hom
               </a>
               <a
                 href="/about"
